f830f3b4e0236d7a373f9d3b1b4a3a073720dbcc
lucasgneccoh <61275066+lucasgneccoh@users.noreply.github.com>
2021-07-13 17:13:48 -0500

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -20,11 +20,9 @@ body = "This mail was sent using Linux and python"
 subj = f' -s "{args.subject}"' if "subject" in args else ""
 attach = ' '.join([f'-a {os.path.join(pwd,s)}' for s in args.attach]) if "attach" in args else ""
 
-print("attach: ", attach)
-    
-sys.exit(1)
-
 command = f'{cmd} {msg} | mail{subj}{attach} {args.to}'
+
+print(command)
 
 tmp_file_name = "tmp_file_mail.sh"
 
