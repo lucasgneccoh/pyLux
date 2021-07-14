6f94b9144a1a9431042a9e92d3200ecefc3771ec
lucasgneccoh <61275066+lucasgneccoh@users.noreply.github.com>
2021-07-13 19:21:06 -0500

    Update run_exp_it_ext.sh

--- core/run_exp_it_ext.sh
+++ core/run_exp_it_ext.sh
@@ -2,9 +2,11 @@
 output_file=$1
 to=$2
 pwd=$(pwd)
-#python3 expert_iteration_external.py --inputs ../support/exp_iter_inputs/exp_iter_inputs.json --verbose 0 > $output_file
-echo "Testing with a created log file" > $output_file
+# Run the script
+python3 expert_iteration_external.py --inputs ../support/exp_iter_inputs/exp_iter_inputs.json --verbose 0 > $output_file
+# Send email
 touch tmp_mail.sh
-echo 'mail -s "expert iteration finished" -A '$pwd'/'$output_file' '$to > tmp_mail.sh
+echo 'echo "Automatic mail" | mail -s "Expert iteration finished" -A '$pwd'/'$output_file' '$to > tmp_mail.sh
 ssh -p 5022 lgnecco@lamgate4 'bash -s' < tmp_mail.sh
+rm tmp_mail.sh
 echo "Done"
