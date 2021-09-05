8f61d097381e34b17b5d005fce0a06cd8f67a096
lucasgneccoh <61275066+lucasgneccoh@users.noreply.github.com>
2021-09-05 12:26:36 +0200

    attack_probs

--- core/probabilities_attacks.py
+++ core/probabilities_attacks.py
@@ -7,24 +7,27 @@ Created on Wed Sep  1 19:05:19 2021
 
 
 import numpy as np
+import pandas as pd
 
-nodes = 4
-R = 150
-theta_offset = np.pi
-H_offset, W_offset = 720/2,1280/2
-
-
-theta = 0
-theta_inc = np.pi * 2 / nodes
-for i in range(nodes):  
-  x = round(np.cos(theta + theta_offset)*R + W_offset,0)
-  y = round(np.sin(theta + theta_offset)*R + H_offset,0)
-  print(f"({x: >5}, {y: >5})")
-  theta += theta_inc
+if False:
+  nodes = 4
+  R = 150
+  theta_offset = np.pi
+  H_offset, W_offset = 720/2,1280/2
   
   
-EI_inputs_path = "../support/exp_iter_inputs/exp_iter_inputs_small.json"
-
+  theta = 0
+  theta_inc = np.pi * 2 / nodes
+  for i in range(nodes):  
+    x = round(np.cos(theta + theta_offset)*R + W_offset,0)
+    y = round(np.sin(theta + theta_offset)*R + H_offset,0)
+    print(f"({x: >5}, {y: >5})")
+    theta += theta_inc
+  
+  
+  
+EI_inputs_path = "../support/exp_iter_inputs/exp_iter_inputs_test.json"
+  
 from board import Board, World , RandomAgent   
 import misc
     
@@ -47,58 +50,45 @@ board.setPreferences(prefs)
 
 
 
+if False:
+  num_tries = 1
+  results = {(0,2):0, (1,1):0, (2,0):0}
+  for _ in range(num_tries):
+    aLoss, dLoss = board.roll(3, 2)
+    results[(aLoss, dLoss)]+=1
+  
+  for k in results:
+    results[k] /= num_tries
+  
+  print("Results of one attack")
+  print(results)
+  print("\n\n")
 
-
-num_tries = 1
-results = {(0,2):0, (1,1):0, (2,0):0}
-for _ in range(num_tries):
-  aLoss, dLoss = board.roll(3, 2)
-  results[(aLoss, dLoss)]+=1
-
-for k in results:
-  results[k] /= num_tries
-
-print("Results of one attack")
-print(results)
-print("\n\n")
-
-num_tries = 50000
 def attack_till_dead(A,D):
-  while A>0 and D>0:
-     aLoss, dLoss = board.roll(min(A,3), min(D,2))
+  while A>1 and D>0:
+     aLoss, dLoss = board.roll(min(A-1,3), min(D,2))
      A -= aLoss
      D -= dLoss
-  if A == 0:
+  if A == 1:
     return 0
   elif D==0:
     return 1
   else:
     return None
+if True:
 
-a_armies, d_armies = 20 , 8 
-
-res = 0
-for _ in range(num_tries):
-  res += attack_till_dead(a_armies, d_armies)
-  
-print(f"Results of attack till dead A = {a_armies}, D = {d_armies}")
-print(f"Attacker wins {res/num_tries} of the times")
-print(f"Defender wins {1 - res/num_tries} of the times")
-print()
-
-aux = a_armies
-a_armies = d_armies
-d_armies = aux
-res = 0
-for _ in range(num_tries):
-  res += attack_till_dead(a_armies, d_armies)
-  
-print(f"Results of attack till dead A = {a_armies}, D = {d_armies}")
-print(f"Attacker wins {res/num_tries} of the times")
-print(f"Defender wins {1 - res/num_tries} of the times")
-print()
-
-
-
-  
-  
+  num_tries = 100000
+  max_A, max_D = 20 , 20 
+  res = [[0 for _ in range(3,max_D+1)] for _ in range(3,max_A+1)]
+  for a in range(3,max_A+1):
+    for d in range(3,max_D+1):
+      aux = 0
+      for _ in range(num_tries):
+        aux += attack_till_dead(a, d)
+        
+      res[a-1][d-1] = aux/num_tries
+      
+      
+  pd.DataFrame(res).to_csv("attack_probs.csv")
+        
+      
